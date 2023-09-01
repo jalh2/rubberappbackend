9d18757cef7cc6deb62c70238ebf778c95eaa901
Dave Jal <dave.aiboostur@gmail.com>
2023-09-01 17:16:00 +0000

    fc4

--- routes/records.js
+++ routes/records.js
@@ -1,14 +1,13 @@
 const express = require('express')
 
 // controller functions
-const { insertRecord, getRecord, getRecords, deleteRecords,deleteOneRecord, incrementTapperCount } = require('../controllers/recordController')
+const { insertRecord, getRecord, getRecords, deleteRecords,deleteOneRecord } = require('../controllers/recordController')
 
 const router = express.Router()
 
 
 // tapper routes
 router.post('/insert', insertRecord)
-router.post('/incrementtapper', incrementTapperCount)
 router.get('/getonerecord/:recordId', getRecord)
 router.get('/getallrecords/:tapperId', getRecords)
 router.get('/deleteallrecords/:id', deleteRecords)
